2edd1417c45868769b03a4657303263105fc44d7
John Naylor <jonaylor89@gmail.com>
2020-02-04 15:35:09 -0500

    serial

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+"""
+
+- Read in images from directory into a queue
+- Serially process images
+    - Convert to numpy array
+    - Apply filters
+        - Salt and pepper
+        - Gaussian noise
+        - Convert to gray scale
+    - Output histograms
+    - Display performance
+- Display performance for batch
+
+"""
+
 def main():
     pass 
 

4d42360fa935425902c37dba67763c704c5d141b
John Naylor <jonaylor89@gmail.com>
2020-02-06 20:11:58 -0500

    who doesn't like color

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 
 from PIL import Image
 from pathlib import Path
+from collections import defaultdict
 from click import echo, style
 
 from typing import List
@@ -19,7 +20,8 @@ def timeit(f):
         te = time.time()
         if "log_time" in kwargs:
             name = kwargs.get("log_name", f.__name__.upper())
-            kw["log_time"][name] = int((te - ts) * 1000)
+            kwargs["log_time"][name] += int((te - ts) * 1000)
+
         else:
             echo(style(f"[DEBUG] {f.__name__}  {((te - ts) * 1000):.2f} ms"))
 
@@ -28,45 +30,58 @@ def timeit(f):
     return timed
 
 
-def calculate_histogram(img_array: np.array):
+def calculate_histogram(img_array: np.array, log_time=None):
     pass
 
 
-def select_color(img_array: np.array) -> np.array:
+def select_color(img_array: np.array, log_time=None) -> np.array:
     pass
 
 
-def season(img_arr: np.array) -> np.array:
+def season(img_arr: np.array, log_time=None) -> np.array:
     pass
 
 
-def gaussian_noise(img_arr: np.array) -> np.array:
+def gaussian_noise(img_arr: np.array, log_time=None) -> np.array:
     pass
 
 
-def linear_filter(img_arr: np.array) -> np.array:
+def linear_filter(img_arr: np.array, log_time=None) -> np.array:
     pass
 
 
-def median_filter(img_arr: np.array) -> np.array:
+def median_filter(img_arr: np.array, log_time=None) -> np.array:
     pass
 
 
 @timeit
-def get_image_data(filename: Path) -> np.array:
+def get_image_data(filename: Path, log_time=None) -> np.array:
     with Image.open(filename) as img:
-        echo(style("[INFO] ", fg="green") + f"extracting data from: {filename}")
+        echo(
+            style("[INFO] ", fg="green")
+            + f"extracting data from: {style(str(filename), fg='cyan')}"
+        )
         return np.array(img)
 
 
 def main(argv: List[str]):
 
     base_path = Path(argv[1])
+    files = list(base_path.glob("*.jpg"))
 
-    for f in base_path.glob("*.jpg"):
-        img = get_image_data(f)
+    time_data = defaultdict(int)
 
-        echo(style(f"[INFO] ", fg="green") + f"image data: {type(img)}")
+    for f in files:
+        img = get_image_data(f, log_time=time_data)
+
+        # echo(style("[INFO] ", fg="green") + f"image data: {type(img)}")
+
+    for k, v in time_data.items():
+        echo(
+            style("[INFO] ", fg="green")
+            + "average time data: "
+            + style(f"{k} : {(v / len(files)):.2f} ms", bold=True, fg="red")
+        )
 
 
 if __name__ == "__main__":

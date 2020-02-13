63cab94ad5f5b7fb4bea5fc6e0cf764b8bfd07cf
John Naylor <jonaylor89@gmail.com>
2020-02-13 16:37:08 -0500

    apply filter

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 
 import sys
 import time
-import numba
 import numpy as np
 
+from numba import jit
 from PIL import Image
 from pathlib import Path
 from click import echo, style
@@ -19,28 +19,15 @@ def timeit(f):
         ts = time.time()
         result = f(*args, **kwargs)
         te = time.time()
-        if "log_time" in kwargs:
-            name = kwargs.get("log_name", f.__name__.upper())
 
-            if name not in kwargs["log_time"].keys():
-                kwargs["log_time"][name] = 0
-
-            kwargs["log_time"][name] += int((te - ts) * 1000)
-
-        else:
-            echo(
-                style(
-                    f"[DEBUG] {f.__name__}  {((te - ts) * 1000):.2f} ms",
-                    fg="red",
-                )
-            )
+        echo(style(f"[DEBUG] {f.__name__}  {((te - ts) * 1000):.2f} ms", fg="red"))
 
         return result
 
     return timed
 
 
-@numba.jit(nopython=True)
+@jit(nopython=True)
 def cumsum(a):
     a = iter(a)
     b = [next(a)]
@@ -50,7 +37,7 @@ def cumsum(a):
     return np.array(b)
 
 
-@numba.jit(nopython=True)
+@jit(nopython=True)
 def histogram(img_array):
     """
     >> h=zeros(256,1);              OR    >> h=zeros(256,1);
@@ -101,12 +88,13 @@ def calculate_histogram(img_array: np.array) -> np.array:
 
     cs = nj / N
 
-    cs = cs.astype('uint8')
+    cs = cs.astype("uint8")
 
     equalized = cs[flat]
     img_new = np.reshape(equalized, img_array.shape)
 
     return hist, histogram(equalized), img_new
+
 
 def select_channel(img_array: np.array, color: str = "", log_time=None) -> np.array:
 
@@ -121,43 +109,87 @@ def select_channel(img_array: np.array, color: str = "", log_time=None) -> np.ar
 
     else:
         # Default to using the default greyscaling Pillow does
-        img = Image.fromarray(img_array, 'L')
+        img = Image.fromarray(img_array, "L")
 
         return np.array(img)
 
 
-@timeit 
+@timeit
 def season(img_arr: np.array, strength: int, log_time=None) -> np.array:
     pass
 
 
 @timeit
-def gaussian_noise(img_arr: np.array, params: int, log_time=None) -> np.array:
-    pass
+@jit(nopython=True)
+def gaussian_filter(img_arr: np.array, params: int) -> np.array:
+
+    filter_size = 5 
+    kernel = np.zeros((filter_size, filter_size))
+
+    somme = 0
+
+    for i in range(filter_size):
+        for j in range(filter_size):
+            x = i - (rows - 1) / 2.0
+            y = j - (cols - 1) / 2.0
+            gauss[i][j] = K * exp(((x ** 2 + y ** 2) / (2 * sigma ** 2) * (-1)))
+
+            somme += gauss[i][j]
+
+    for i in range(filter_size):
+        for j in range(filter_size):
+            gauss[i][j] /= somme
+
+
+    gauss = apply_filter(img_arr, kernel)
+
+    return gauss
 
 
 @timeit
 def linear_filter(
-    img_arr: np.array, mask_size: int, weights: List[List[int]], log_time=None
+    img_arr: np.array, mask_size: int, weights: List[List[int]]
 ) -> np.array:
-    pass
+
+    linear = apply_filter(img_size)
 
 
 @timeit
 def median_filter(
-    img_arr: np.array, mask_size: int, weights: List[List[int]], log_time=None
+    img_arr: np.array, mask_size: int, weights: List[List[int]]
 ) -> np.array:
-    pass
+
+    median = apply_filter(img_arr, median)
+
+
+@jit(nopython=True)
+def apply_filter(img_arr, filter):
+    rows, cols = imgs.shape
+    height, width = filters.shape
+
+    output = np.zeros((cols - height + 1, rows - width + 1))
+
+    for rr in range(n_rows - height + 1):
+        for cc in range(n_cols - width + 1):
+            for hh in range(height):
+                for ww in range(width):
+                    imgval = img_arr[rr + hh, cc + ww]
+                    filterval = filter[hh, ww]
+                    output[rr, cc] += imgval * filterval
+
+    return output
 
 
 def export_image(img_arr: np.array, filename: str) -> None:
     img = Image.fromarray(img_arr)
     img.save(filename)
 
+
 def export_plot(img_arr: np.array, filename: str) -> None:
     _ = plt.hist(img_arr, bins=256, range=(0, 256))
     plt.title(filename)
     plt.savefig(filename + ".png")
+
 
 @timeit
 def get_image_data(filename: Path, log_time=None) -> np.array:
@@ -192,11 +224,11 @@ def main(argv: List[str]):
         # echo(style("[INFO] ", fg="green") + f"image data: {type(img)}")
 
         img = select_channel(color_img, color="red")
-        
+
         salt_and_pepper = season(img, 5, log_time=time_data)
         # export_image(salt_and_pepper, "salt_and_pepper_" + f)
 
-        guass = gaussian_noise(img, 5, log_time=time_data)
+        guass = gaussian_filter(img, 5, log_time=time_data)
         # export_image(guass, "guassian_" + f)
 
         linear = linear_filter(img, 9, [[0]], log_time=time_data)
@@ -208,7 +240,6 @@ def main(argv: List[str]):
         histogram, equalized, _ = calculate_histogram(img)
         # export_plot(histogram, "datasets/output/histogram_" + f.stem)
         # export_plot(equalized, "datasets/output/histogram_equalized_" + f.stem)
-
 
         # calculate_histogram(img, log_time=time_data)
         # histrogram_equalization(img, log_time=time_data)
@@ -222,8 +253,8 @@ def main(argv: List[str]):
             + style(f"{k} : {(v / len(files)):.2f} ms", fg="red")
         )
 
-
     echo(style(f"[INFO] Total time: {t_delta}", fg="red"))
+
 
 if __name__ == "__main__":
     main(sys.argv)
